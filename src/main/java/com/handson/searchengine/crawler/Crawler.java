4a63b018ef99a1334ba066dd19b39edad8c4666d
elad9219 <elad9219@gmail.com>
2025-08-05 16:07:50 +0300

    with elasticsearch

--- src/main/java/com/handson/searchengine/crawler/Crawler.java
+++ src/main/java/com/handson/searchengine/crawler/Crawler.java
@@ -2,7 +2,9 @@ package com.handson.searchengine.crawler;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.handson.searchengine.kafka.Producer;
 import com.handson.searchengine.model.*;
+import com.handson.searchengine.util.ElasticSearch;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jsoup.Jsoup;
@@ -28,28 +30,30 @@ public class Crawler {
 
     @Autowired
     ObjectMapper om;
+    
+    @Autowired
+    Producer producer;
+
+    @Autowired
+    ElasticSearch elasticSearch;
 
     protected final Log logger = LogFactory.getLog(getClass());
-
-    public static final int MAX_CAPACITY = 100000;
-    private BlockingQueue<CrawlerRecord> queue = new ArrayBlockingQueue<CrawlerRecord>(MAX_CAPACITY);
+    
     public void crawl(String crawlId, CrawlerRequest crawlerRequest) throws InterruptedException, IOException {
         initCrawlInRedis(crawlId);
-        queue.clear();
-        queue.put(CrawlerRecord.of(crawlId, crawlerRequest));
-        while (!queue.isEmpty() && getStopReason(queue.peek()) == null) {
-            CrawlerRecord rec = queue.poll();
-            logger.info("crawling url:" + rec.getUrl());
-            setCrawlStatus(crawlId, CrawlStatus.of(rec.getDistance(), rec.getStartTime(), 0, null));
+        producer.send(CrawlerRecord.of(crawlId, crawlerRequest));
+    }
+
+    public void crawlOneRecord(String crawlId, CrawlerRecord rec) throws IOException, InterruptedException {
+        logger.info("crawling url:" + rec.getUrl());
+        StopReason stopReason = getStopReason(rec);
+        setCrawlStatus(crawlId, CrawlStatus.of(rec.getDistance(), rec.getStartTime(), 0, stopReason));
+        if (stopReason == null) {
             Document webPageContent = Jsoup.connect(rec.getUrl()).get();
+            indexElasticSearch(rec, webPageContent);
             List<String> innerUrls = extractWebPageUrls(rec.getBaseUrl(), webPageContent);
             addUrlsToQueue(rec, innerUrls, rec.getDistance() +1);
         }
-
-        CrawlerRecord rec = queue.peek();
-        var stopReason =  getStopReason(rec);
-        setCrawlStatus(crawlId, CrawlStatus.of(rec.getDistance(), rec.getStartTime(), 0, stopReason));
-
     }
 
     private StopReason getStopReason(CrawlerRecord rec) {
@@ -60,11 +64,11 @@ public class Crawler {
     }
 
 
-    private void addUrlsToQueue(CrawlerRecord rec, List<String> urls, int distance) throws InterruptedException {
+    private void addUrlsToQueue(CrawlerRecord rec, List<String> urls, int distance) throws InterruptedException, JsonProcessingException {
         logger.info(">> adding urls to queue: distance->" + distance + " amount->" + urls.size());
         for (String url : urls) {
             if (!crawlHasVisited(rec, url)) {
-                queue.put(CrawlerRecord.of(rec).withUrl(url).withIncDistance()) ;
+                producer.send(CrawlerRecord.of(rec).withUrl(url).withIncDistance()); ;
             }
         }
     }
@@ -78,6 +82,13 @@ public class Crawler {
         logger.info(">> extracted->" + links.size() + " links");
 
         return links;
+    }
+
+    private void indexElasticSearch(CrawlerRecord rec, Document webPageContent) {
+        logger.info(">> adding elastic search for webPage: " + rec.getUrl());
+        String text = String.join(" ", webPageContent.select("a[href]").eachText());
+        UrlSearchDoc searchDoc = UrlSearchDoc.of(rec.getCrawlId(), text, rec.getUrl(), rec.getBaseUrl(), rec.getDistance());
+        elasticSearch.addData(searchDoc);
     }
 
     private void initCrawlInRedis(String crawlId) throws JsonProcessingException {
