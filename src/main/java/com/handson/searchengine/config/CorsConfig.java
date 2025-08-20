807066778d939d36120ca6d6d2b2732969801d00
elad9219 <elad9219@gmail.com>
2025-08-20 14:13:31 +0300

    CorsConfig.java deleted

---
+++ src/main/java/com/handson/searchengine/config/CorsConfig.java
@@ -0,0 +1,22 @@
+/*
+package com.handson.searchengine.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins(
+                "https://search.runmydocker-app.com", "http://localhost:3000",
+                        null).allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
+
+ */
